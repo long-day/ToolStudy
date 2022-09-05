727c81132ae59fc58585c366ec93e2a5f1526002
jun <rosyevening@qq.com>
2022-09-05 17:15:21 +0800

    单例模式

--- src/test/java/com/longday/otherTools/designPatterns/createDesignMode/SingletonTest.java
+++ src/test/java/com/longday/otherTools/designPatterns/createDesignMode/SingletonTest.java
@@ -3,14 +3,22 @@ package com.longday.otherTools.designPatterns.createDesignMode;
 import lombok.Data;
 import org.junit.jupiter.api.Test;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * @author 君
  * @version 1.0
  * @date 2022/9/5
  */
 public class SingletonTest {
-// 饿汉式推荐使用 枚举方式
-//懒汉式 推荐使用 静态内部类方式，当然双重检查锁方式也可。
+// 饿汉式中枚举方式会浪费一定的内存空间但是由于其实JVM底层的实现，所以可以防止单例模式被破坏(天然自带)
+// 懒汉式 推荐使用 静态内部类方式，当然双重检查锁方式也可。
+// 破坏单例模式的方法: 序列化和反射
     @Test
     public void hungryModel(){
         SingletonHungry clazz1 = SingletonHungry.getHungrySingleton();
@@ -64,13 +72,57 @@ public class SingletonTest {
                       true
          */
     }
+    @Test
+    public void destructionSingletonModelByReflect() {
+        //运行此方法时请将SingletonHungry类上的@Data注解注释掉，要不然不会打印出地址值
+        Class<?> singleton = SingletonHungry.class;
+        try {
+            Constructor<?> noArgsConstruction = singleton.getDeclaredConstructor();
+            noArgsConstruction.setAccessible(true);
+            Object o = noArgsConstruction.newInstance();
+            Object o1 = noArgsConstruction.newInstance();
+            System.out.println(o);//SingletonHungry@27c6e487
+            System.out.println(o1);//SingletonHungry@49070868
+            System.out.println(o == o1);//false
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //解决方案
+        /*走构造方法这样写,直接不让他创建:
+         private SingletonHungry(){
+            throw new RuntimeException("请不要创建多个对象！(小比崽子,用反射是吧？不要脸 -.-)");
+         }
+        */
+    }
+
+    @Test
+    public void destructionSingletonModelByDeserialize() throws Exception {
+        //运行此方法时请将SingletonHungry类上的@Data注解注释掉，要不然不会打印出地址值
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(Paths.get("src/test/java/com/longday/otherTools/designPatterns/createDesignMode/files/temp.txt")));
+        objectOutputStream.writeObject(SingletonHungry.getHungrySingleton());
+        objectOutputStream.close();
+
+        ObjectInputStream inputStream = new ObjectInputStream(Files.newInputStream(Paths.get("src/test/java/com/longday/otherTools/designPatterns/createDesignMode/files/temp.txt")));
+        SingletonHungry o = (SingletonHungry) inputStream.readObject();
+        inputStream.close();
+
+        inputStream = new ObjectInputStream(Files.newInputStream(Paths.get("src/test/java/com/longday/otherTools/designPatterns/createDesignMode/files/temp.txt")));
+        SingletonHungry o1 = (SingletonHungry) inputStream.readObject();
+        inputStream.close();
+        System.out.println(o);//SingletonHungry@dd3b207
+        System.out.println(o1);//SingletonHungry@551bdc27
+        System.out.println(o == o1); //false
+
+        //解决方案
+        //在被序列化类中添加readResolve方法后，返回为true
+    }
 }
 
 /**
  * 饿汉式
  */
-@Data
-class SingletonHungry{
+//@Data
+class SingletonHungry implements Serializable {
     private static SingletonHungry singletonHungry = new SingletonHungry();
     private String info;
     private String title;
@@ -78,9 +130,18 @@ class SingletonHungry{
      * 单例模式不对外提供构造方法
      */
     private SingletonHungry(){
+      throw new RuntimeException("请不要创建多个对象！(小比崽子,用反射是吧？不要脸 -.-)");
     }
 
     public static SingletonHungry getHungrySingleton(){
+        return singletonHungry;
+    }
+
+    /**
+     * 反序列化规则，设置反序列化时对象的获取来源。没有此方法默认为new一个新对象。
+     * @return 单例对象
+     */
+    public Object readResolve(){
         return singletonHungry;
     }
 }

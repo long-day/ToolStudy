4a35dc93cee29ec708ea42d8f409f8835d388fbb
jun <rosyevening@qq.com>
2022-09-06 17:48:58 +0800

    原型模式

---
+++ src/test/java/com/longday/otherTools/designPatterns/createDesignMode/PrototypeTest.java
@@ -0,0 +1,15 @@
+package com.longday.otherTools.designPatterns.createDesignMode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 君
+ * @version 1.0
+ * @date 2022/9/6
+ */
+public class PrototypeTest {
+    @Test
+    public void prototype(){
+
+    }
+}

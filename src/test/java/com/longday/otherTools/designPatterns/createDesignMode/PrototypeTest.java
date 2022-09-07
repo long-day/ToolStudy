02336356f99d75360bb627688cce52f17e79adb4
jun <rosyevening@qq.com>
2022-09-07 10:26:36 +0800

    原型模式

--- src/test/java/com/longday/otherTools/designPatterns/createDesignMode/PrototypeTest.java
+++ src/test/java/com/longday/otherTools/designPatterns/createDesignMode/PrototypeTest.java
@@ -1,6 +1,9 @@
 package com.longday.otherTools.designPatterns.createDesignMode;
 
+import com.google.gson.Gson;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
 
 /**
  * @author 君
@@ -9,7 +12,63 @@ import org.junit.jupiter.api.Test;
  */
 public class PrototypeTest {
     @Test
-    public void prototype(){
+    public void prototype() throws CloneNotSupportedException {
+        RealizeType realizeType = new RealizeType();
+        RealizeType clone = realizeType.clone();
+        System.out.println(clone == realizeType);
+
+        //使用Spring BeanUtils
+        Actor actor = new Actor();
+        actor.setName("Julia Butters");
+        Movie movie = new Movie();
+        movie.setActor(actor);
+
+        Movie newMovie = new Movie();
+        BeanUtils.copyProperties(movie, newMovie);
+
+        System.out.println(movie.getActor() == newMovie.getActor()); //true
+
+        //使用Cglib BeanCopier 同上
 
     }
+
+    @Test
+    public void prototypeDeep() {
+        Actor actor = new Actor();
+        actor.setName("Julia Butters");
+        Movie movie = new Movie();
+        movie.setActor(actor);
+
+        //Gson
+        Gson gson = new Gson();
+        Movie newMovie = gson.fromJson(gson.toJson(movie), Movie.class);
+        newMovie.getActor().setName("新");
+        System.out.println(movie.getActor().getName());
+        System.out.println(newMovie.getActor().getName());
+        System.out.println(movie.getActor() == newMovie.getActor()); //true
+
+
+    }
+}
+
+class RealizeType implements Cloneable {
+    public RealizeType() {
+        System.out.println("无参构造方法");
+    }
+
+    @Override
+    public RealizeType clone() throws CloneNotSupportedException {
+        return (RealizeType) super.clone();
+    }
+}
+
+@Data
+class Movie {
+    private Actor actor;
+
+}
+
+@Data
+class Actor {
+    private String name;
 }

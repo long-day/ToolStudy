d1373e5c7f02f74b9dc9e741315987a9d5df39a5
jun <rosyevening@qq.com>
2022-09-07 22:40:59 +0800

    装饰着模式

---
+++ src/test/java/com/longday/otherTools/designPatterns/structuralMode/DecoratorTest.java
@@ -0,0 +1,15 @@
+package com.longday.otherTools.designPatterns.structuralMode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 君
+ * @version 1.0
+ * @date 2022/9/7
+ */
+public class DecoratorTest {
+    @Test
+    public void decorator(){
+
+    }
+}

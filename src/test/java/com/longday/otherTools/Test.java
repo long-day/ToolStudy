19a2e3475b132819a285ba35de2383c290f4f5fe
jun <rosyevening@qq.com>
2022-09-16 18:24:44 +0800

    工具类

---
+++ src/test/java/com/longday/otherTools/Test.java
@@ -0,0 +1,19 @@
+package com.longday.otherTools;
+
+/**
+ * @author 君
+ * @version 1.0
+ * @desc 工具类 测试普通代码
+ * @date 2022/9/14
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Demo{
+    public Demo(){
+
+    }
+}
